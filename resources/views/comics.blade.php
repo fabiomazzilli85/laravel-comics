dc865df33853649803e6f046cb4e5c19d159d796
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-21 17:25:03 +0200

    In content impostate a comics stesse classi bootstrap impostate a home.

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Comics')
 
 @section('content')
-    <div class="container mt-5 mb-5">
-        <h1>Comics</h1>
-        <p>Contenuto della pagina Comics...</p>
+    <div class="container mt-5 mb-5 text-center">
+        <h1 class="text-warning">Comics</h1>
+        <p class="text-danger">Contenuto della pagina Comics...</p>
     </div>
 @endsection
