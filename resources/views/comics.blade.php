e0c6c3ad937f8b3830cbf87f45cd6cbffff65a8b
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-22 17:20:47 +0200

    Rinominata classe in flex-img.

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,18 +4,18 @@
 
 @section('content')
 @foreach ($comics as $comic)
-    <div class="container">
-        <div class="flex">
-            <img src="{{ $comic['thumb'] }}" alt="">
-            <div>
-                <ul>
-                    <li class="text-primary"><strong>Titolo</strong>: {{ $comic['title'] }}</li>
-                </ul>
-                <ul>
-                    <li class="text-success"><strong>Serie</strong>: {{ $comic['series'] }}</li>
-                </ul>
-            </div>
+<div class="container">
+    <div class="flex-img">
+        <img src="{{ $comic['thumb'] }}" alt="">
+        <div>
+            <ul>
+                <li class="text-primary"><strong>Titolo</strong>: {{ $comic['title'] }}</li>
+            </ul>
+            <ul>
+                <li class="text-success"><strong>Serie</strong>: {{ $comic['series'] }}</li>
+            </ul>
         </div>
     </div>
+</div>
 @endforeach
 @endsection
