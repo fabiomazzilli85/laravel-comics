5d00b1d93459b2e2bb731906cc713dfb396bf019
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-22 17:44:47 +0200

    Da card rimosso h2 e inserito p con titolo in strong.

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -8,7 +8,7 @@
         <div class="card mb-5" style="width: 18rem">
             <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="">
             <div class="card-body">
-                <h5 class="card-title text-center">{{ $comic['title'] }}</h5>
+                <p class="card-title text-center"><strong>Titolo</strong>: {{ $comic['title'] }}</p>
                 <p class="card-text text-center"><strong>Serie</strong>: {{ $comic['series'] }}</p>
             </div>
         </div>
