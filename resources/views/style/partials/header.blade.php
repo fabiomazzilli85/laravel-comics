ff96e8158c339410705b96e761e540f5cb3d6eb4
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-22 17:21:36 +0200

    Inserita classe ad anchor.

--- resources/views/style/partials/header.blade.php
+++ resources/views/style/partials/header.blade.php
@@ -1,15 +1,15 @@
 <header>
     <div class="container">
         <nav>
-            <ul class="flex-nav">
+            <ul class="flex-nav nav-bg">
                 <li>
-                    <a href="http://localhost:8001/">Home</a>
+                    <a class="link-item" href="http://localhost:8001/">Home</a>
                 </li>
                 <li>
-                    <a href="http://localhost:8001/comics">Comics</a>
+                    <a class="link-item" href="http://localhost:8001/comics">Comics</a>
                 </li>
             </ul>
         </nav>
     </div>
-    <h1 class="text-center mt-3">Laravel Project</h1>
+    <h1 class="text-center mt-5 text-danger">Laravel Project</h1>
 </header>
