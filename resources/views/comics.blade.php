a185b4a79d12e4d78cb02b993f3a2859470c8248
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-22 16:31:48 +0200

    Inserita stringa Titolo e Serie nella Li.

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,12 +4,14 @@
 
 @section('content')
 @foreach ($comics as $comic)
-    <img src="{{ $comic['thumb'] }}" alt="">
-    <ul>
-        <li>{{ $comic['title'] }}</li>
-    </ul>
-    <ul>
-        <li>{{ $comic['series'] }}</li>
-    </ul>
+    <div>
+        <img src="{{ $comic['thumb'] }}" alt="">
+        <ul>
+            <li><strong>Titolo</strong>: {{ $comic['title'] }}</li>
+        </ul>
+        <ul>
+            <li><strong>Serie</strong>: {{ $comic['series'] }}</li>
+        </ul>
+    </div>
 @endforeach
 @endsection
